642fd6363305849146185c9090e9ba9a96f8f401
samin06 <samin06@gmail.com>
2016-07-04 14:55:15 -0700

    Added a third line.

--- mainGitHubTest.cpp
+++ mainGitHubTest.cpp
@@ -4,6 +4,7 @@ int main()
 {
 	std::cout << "Test file for github." << std::endl;
 	std::cout << "Here is another change" << std::endl;
+	std::cout << "Yet another change" << std::endl;
 
 	return 0;
 }
